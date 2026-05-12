bd46e68d237d6a8963d866f75672dfc92394df51
nffdev <nightfrost2discord@outlook.fr>
2026-05-12 16:45:10 +0200

    refactor(core): moved difficulty to core

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'core/theme/app_theme.dart';
-import 'screens/splash/splash_screen.dart';
+import 'features/splash/splash_screen.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
